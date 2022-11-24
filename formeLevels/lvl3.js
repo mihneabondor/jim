b407ff5309e3748bc9af31eaf9fa09b99e38ab7c
mihneabondor <mihnea.bondor@icloud.com>
2022-11-24 20:24:19 +0200

    nu merge

--- formeLevels/lvl3.js
+++ formeLevels/lvl3.js
@@ -1,49 +1,51 @@
 
-var x;
-var y;
-
-var img1X = 0, img2X = 0;
-var img1Y = 0, img2Y = 0;
-var img1, img2;
-
-// loading up an image
-function preload() {
-  img1 = loadImage('../Resources/triunghi.png');
-  img2= loadImage('../Resources/triunghi275.png');
-}
-
-
-// if the mouse pressed when on the image 
-function mouseDragged() {
-  if ((mouseX > img1X + 50) && (mouseX < img1X + 200)) {
-    if ((mouseY > img1Y + 100) && (mouseY < img1Y + 200)) {
-      img1X = mouseX;
-      img1Y = mouseY;
-    }
-  }
-
-  if ((mouseX > img2X - 50) && (mouseX < img2X + 50)) {
-    if ((mouseY > img2Y - 50) && (mouseY < img2Y + 50)) {
-      img2X = mouseX;
-      img2Y = mouseY;
-    }
-  }
-}
-
-function setup() {
-  var canvas=createCanvas(windowWidth/2, windowHeight/1.2);
-    canvas.position(windowWidth/4,windowHeight/10);
-}
-
-function draw() {
-  background('#d3d3d3');
-  line (-1000,400,1000,400);
+$(document).ready(function() {
   
-  stroke('purple');
-    strokeWeight(10);
-    point(img1X+50, img1Y+100);
-    point(img1X+200, img1Y+200);
-    image(img1, img1X-50, img1Y-5, 200, 200);
-//   image(img2, img2X, img2Y, 200, 200);       
-    rect(img1X, img1Y);
-}
+  var currentBox = 3;
+
+  function magnetSnap(speed) {
+    var snapspeed = speed;
+    var p = $(".container div:nth-child(" + currentBox + ")");
+    var offset = p.offset();
+
+    $("#dragbox").animate(
+      {
+        top: offset.top,
+        left: offset.left
+      },
+      snapspeed
+    );
+  }
+
+  magnetSnap(0);
+
+  $(window).resize(function() {
+    magnetSnap(0);
+  });
+
+  $("#dragbox")
+    .mousedown(function() {
+      $(this).addClass("draggingbox");
+    })
+    .mouseup(function() {
+      $(this).removeClass("draggingbox");
+    });
+
+  $("#dragbox").draggable();
+
+  $(".dropbox").droppable({
+    classes: {
+      "ui-droppable-hover": "hoveringbox"
+    },
+
+    over: function(event, ui) {
+      var p = $(this);
+      currentBox = p.index() + 1;
+      $("#dragbox p").text(currentBox);
+    },
+
+    drop: function(event, ui) {
+      magnetSnap(40);
+    }
+  });
+});
