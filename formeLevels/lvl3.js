626ecd85054be60c390c3abdd0acb968f670b365
mihneabondor <mihnea.bondor@icloud.com>
2022-11-24 22:10:26 +0200

    lvl3 cu snap

--- formeLevels/lvl3.js
+++ formeLevels/lvl3.js
@@ -1,51 +1,19 @@
+function setup() {
+  var canvas=createCanvas(windowWidth/2, windowHeight/1.2);
+  canvas.position(windowWidth/4,windowHeight/10);
+  canvas.id("canvas");
+}
 
-$(document).ready(function() {
-  
-  var currentBox = 3;
+function draw() {
+  background("#d3d3d3");
 
-  function magnetSnap(speed) {
-    var snapspeed = speed;
-    var p = $(".container div:nth-child(" + currentBox + ")");
-    var offset = p.offset();
+  line (-1000,400,1000,400);
 
-    $("#dragbox").animate(
-      {
-        top: offset.top,
-        left: offset.left
-      },
-      snapspeed
-    );
-  }
+  setLineDash([10]);
+  fill(175, 200);
+  // let rectangle = rect(windowWidth/6, windowHeight/9, windowWidth/5.75, windowHeight/4);
+}
 
-  magnetSnap(0);
-
-  $(window).resize(function() {
-    magnetSnap(0);
-  });
-
-  $("#dragbox")
-    .mousedown(function() {
-      $(this).addClass("draggingbox");
-    })
-    .mouseup(function() {
-      $(this).removeClass("draggingbox");
-    });
-
-  $("#dragbox").draggable();
-
-  $(".dropbox").droppable({
-    classes: {
-      "ui-droppable-hover": "hoveringbox"
-    },
-
-    over: function(event, ui) {
-      var p = $(this);
-      currentBox = p.index() + 1;
-      $("#dragbox p").text(currentBox);
-    },
-
-    drop: function(event, ui) {
-      magnetSnap(40);
-    }
-  });
-});
+function setLineDash(list) {
+  drawingContext.setLineDash(list);
+}
