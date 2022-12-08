8838d29c50eef25c565abc3aa233f4cfbef3b5f9
mihneabondor <mihnea.bondor@icloud.com>
2022-12-08 19:47:42 +0200

    Squashed commit of the following:
    commit ca8bf9d7a32af286a6bae3cf66d73f9c232dffc0
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Thu Dec 8 19:46:05 2022 +0200
    
        Update calculator.html
    
    commit c2ffc68c6d3b5ef18fa2cf17c083c9aafe264f6f
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Sun Dec 4 00:06:10 2022 +0200
    
        fix
    
    commit 3f2f4897464f8f5aabda390c6f9baa496acc7326
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Sat Dec 3 23:19:15 2022 +0200
    
        Revert "Merge lvl3"
    
        This reverts commit 792cb6a9ceb6975b57003fa1fa2ed317f3296adb.
    
    commit 792cb6a9ceb6975b57003fa1fa2ed317f3296adb
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Fri Nov 25 21:23:22 2022 +0200
    
        Merge lvl3
    
    commit 899059009882c4e4aa5fbd9fa3df20f384d3a90d
    Merge: d7ea596 87fadbf
    Author: Mihnea <73481899+mihneabondor@users.noreply.github.com>
    Date:   Thu Nov 24 22:14:56 2022 +0200
    
        Merge pull request #1 from mihneabondor/forme-lvl-3-cu-Draggable
    
        Forme lvl 3 cu draggable
    
    commit 87fadbfc27542db567de7347bdd7c6dfba414370
    Merge: 626ecd8 d7ea596
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Thu Nov 24 22:13:40 2022 +0200
    
        Merge branch 'forme-lvl3' into forme-lvl-3-cu-Draggable
    
    commit 626ecd85054be60c390c3abdd0acb968f670b365
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Thu Nov 24 22:10:26 2022 +0200
    
        lvl3 cu snap
    
    commit d7ea5963a43e8a1a0c070d8a661b60346dde74d6
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Thu Nov 24 20:40:01 2022 +0200
    
        updated lvl3
    
    commit b407ff5309e3748bc9af31eaf9fa09b99e38ab7c
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Thu Nov 24 20:24:19 2022 +0200
    
        nu merge
    
    commit 83bae011bbfff6c67e2d89eda3af7abcdd15fad6
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Thu Nov 24 20:07:58 2022 +0200
    
        Update lvl3.js
    
    commit 8af2807c742476646741ff5a3e0338452be18fc6
    Author: mihneabondor <mihnea.bondor@icloud.com>
    Date:   Tue Nov 22 23:34:50 2022 +0200
    
        Clasele facute

--- formeLevels/lvl3.js
+++ formeLevels/lvl3.js
@@ -1,49 +1,19 @@
-
-var x;
-var y;
-
-var img1X = 0, img2X = 0;
-var img1Y = 0, img2Y = 0;
-var img1, img2;
-
-// loading up an image
-function preload() {
-  img1 = loadImage('../Resources/triunghi.png');
-  img2= loadImage('../Resources/triunghi275.png');
-}
-
-
-// if the mouse pressed when on the image 
-function mouseDragged() {
-  if ((mouseX > img1X + 50) && (mouseX < img1X + 200)) {
-    if ((mouseY > img1Y + 100) && (mouseY < img1Y + 200)) {
-      img1X = mouseX;
-      img1Y = mouseY;
-    }
-  }
-
-  if ((mouseX > img2X - 50) && (mouseX < img2X + 50)) {
-    if ((mouseY > img2Y - 50) && (mouseY < img2Y + 50)) {
-      img2X = mouseX;
-      img2Y = mouseY;
-    }
-  }
-}
-
 function setup() {
   var canvas=createCanvas(windowWidth/2, windowHeight/1.2);
-    canvas.position(windowWidth/4,windowHeight/10);
+  canvas.position(windowWidth/4,windowHeight/10);
+  canvas.id("canvas");
 }
 
 function draw() {
-  background('#d3d3d3');
+  background("#d3d3d3");
+
   line (-1000,400,1000,400);
-  
-  stroke('purple');
-    strokeWeight(10);
-    point(img1X+50, img1Y+100);
-    point(img1X+200, img1Y+200);
-    image(img1, img1X-50, img1Y-5, 200, 200);
-//   image(img2, img2X, img2Y, 200, 200);       
-    rect(img1X, img1Y);
+
+  setLineDash([10]);
+  fill(175, 200);
+  // let rectangle = rect(windowWidth/6, windowHeight/9, windowWidth/5.75, windowHeight/4);
+}
+
+function setLineDash(list) {
+  drawingContext.setLineDash(list);
 }
