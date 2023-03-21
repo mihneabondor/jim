c44d9005bd39aa4a7eb6f88f814707874b17566c
luca <80474247+luca7ok@users.noreply.github.com>
2023-03-21 20:48:57 +0200

    forme
    formeforme

---
+++ var/www/html/levelType2.js
@@ -0,0 +1,91 @@
+function validateAnswer() {
+    let value = document.getElementById('solution-input').value
+    if(localStorage.getItem('ans') == value) {
+        document.getElementById('shape-container').style.transform = 'translate(0vw, 0vh)'
+        document.getElementById('next-button').style.visibility = 'visible'
+        document.getElementById('next-button').style.opacity = 1
+    }
+}
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+function star(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+function setup()
+{
+    var canvas = createCanvas(windowWidth/2.1,windowHeight/2.8)
+    canvas.parent('shape-container')
+    translate((windowWidth/2.1)/2, (windowHeight/2.8)/2)
+    let types=['3','4','5','6','7','8','9','10'];
+    let col=["rgb(254, 199, 74)","rgb(98, 163, 255)"];
+    let type=random(types);
+    localStorage.setItem('ans',type);
+    if(type=='4')
+    {
+        let types2=['1','2','3'];
+        let type2=random(types2);
+        fill(random(col));
+        if(type2=='1')
+            polygon(0,0,82,4);
+        else if(type2=='2')
+            {
+                rotate(PI/4);
+                polygon(0,0,100,4);
+            }
+        else
+            {
+                rect(-150, -75, 300, 150);
+            }
+    }
+    else if(type=='10')
+    {
+        let types2=['1','2','3'];
+        let type2=random(types2);
+        fill(random(col));
+        if(type2=='1')
+         rotate(PI/2+PI),star(0,0,50,116,5);
+        else
+        rotate(PI/2+PI),polygon(0,0,100,10);
+    }
+    else if(type=='12')
+    {
+        fill(random(col));
+        rotate(PI/4);
+        polygon(0,0,30,4);
+        polygon(30,30,30,4);
+        polygon(60,-60,30,4);
+        polygon(-30,-30,30,4);
+        //pula-n gura nush sa fac o cruce
+
+    }
+    else 
+    {
+        rotate(PI/2+PI);
+        fill(random(col));
+        polygon(0,0,90,type);
+    }
+    
+}
--- var/www/html/newForme.js
+++ var/www/html/newForme.js
@@ -1,64 +1,20 @@
-function validateAnswer() {
-    let value = document.getElementById('solution-input').value
-    if(localStorage.getItem('answer') == value) {
-        document.getElementById('shape-container').style.transform = 'translate(0vw, 0vh)'
-        document.getElementById('next-button').style.visibility = 'visible'
-        document.getElementById('next-button').style.opacity = 1
-    }
-}
 
-function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-function setupVars() {
-    let numberOfAngles = getRandomArbitrary(3, 5), angles = [], sum = 0
-    for(let i = 0; i < numberOfAngles; i++) {
-        let angle = getRandomArbitrary(60, 360/numberOfAngles)
-        angle -= angle%5
-        angles.push(Number(angle))
-    }
-    angles.sort(function(a, b){return a - b});
-    console.log(angles)
-    for(let elem of angles) {sum += elem}
-    localStorage.setItem('angles', angles)
-    localStorage.setItem('answer', 360-sum)
-    return angles
-}
-
-function setup() {
-    var canvas = createCanvas(windowWidth/2.1,windowHeight/2.8)
-    canvas.parent('shape-container')
-    translate((windowWidth/2.1)/2, (windowHeight/2.8)/2)
-    fill('violet')
-    circle(0, 0, 200)
-
-    let anglesArray = setupVars(), pastSum = anglesArray[0], colors = ['#DB5375', '#EC9192', '#95B67F', '#B5BD89', '#729EA1']
-    let r = 50, theta = 0.75;
-    let x = r * cos(theta);
-    let y = r * sin(theta);
-
-    fill(colors[0])
-    arc(0, 0, 200, 200, radians(0), radians(pastSum))
-
-    textSize(16);
-    fill('black')
-    text(anglesArray[0], x, y)
-
-    for(let i = 1; i < anglesArray.length; i++) {
-        fill(colors[i])
-        arc(0, 0, 200, 200, radians(pastSum), radians(pastSum+anglesArray[i]))
-        x = r * cos(radians(pastSum)+0.7);
-        y = r * sin(radians(pastSum)+0.3);
-        fill('black')
-        text(anglesArray[i], x, y)
-        theta += 0.75
-        pastSum += anglesArray[i-1]
+function randomize(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
 
-    x = r * cos(radians(pastSum)+1.5);
-    y = r * sin(radians(pastSum)+0.5);
-    fill('black')
-    text('?', x, y)
+    return array;
+  }
 
+const lvl=["1","2"];
+function newPageForme()
+{
+    const shuffledLevels=randomize(lvl);
+    var page="levelType"+shuffledLevels[0]+".html";
+    window.open(page,"_self");
 }
